f72ca2c95a1564ff986c3b7280149ac5f1642fbf
Makki <111205775+MakkiMuzisaMuhamadi@users.noreply.github.com>
2024-02-29 07:41:03 -0800

    Update settings.py

--- appliances_uganda/settings.py
+++ appliances_uganda/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-um5+h5uo(2$ujx+!m9@h(kv9yz#xl-*7s+5$l9yfj65+nm@m0*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
