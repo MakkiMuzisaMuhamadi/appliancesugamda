8c2b48490778c76e1f0ba692325947339c7a8296
unknown <muhamadimuzisamakki@gmail.com>
2024-03-01 22:25:20 -0800

    'ggg'

--- mainApp/models.py
+++ mainApp/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/', default='')  # Assuming you want to upload category images
     name = models.CharField(max_length=100)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=100, decimal_places=0)
     description = models.TextField()
     def __str__(self):
         return self.name
